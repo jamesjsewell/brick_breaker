9b33904de019780a0667700867a674bab248a75c
james sewell <jamesjosephsewell@gmail.com>
2020-07-26 02:02:41 -0500

    adds paddle and ball

---
+++ Assets/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        rb.AddForce(Vector2.up * 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("bottom")){
+            Debug.Log("Ball hit the bottom of the screen");
+        }
+    }
+}
