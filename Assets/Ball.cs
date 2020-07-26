8486eff4d84828d6c00139c0fb3d9c87195ea0c2
james sewell <jamesjosephsewell@gmail.com>
2020-07-26 15:34:56 -0500

    adds bricks

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -5,23 +5,41 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D> ();
-        rb.AddForce(Vector2.up * 500);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!inPlay){
+            transform.position = paddle.position;
+        }
+
+        if(Input.GetButtonDown("Jump") && !inPlay){
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("bottom")){
             Debug.Log("Ball hit the bottom of the screen");
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.transform.CompareTag("brick"))  {
+            Destroy(other.gameObject);
         }
     }
 }
